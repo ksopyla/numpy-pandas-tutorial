220299829420b240eddf54c1a6ea654f0cf429e6
ksopyla <sopyla@ermlab.com>
2018-01-21 10:25:15 +0100

    add ploting

---
+++ 2.Pandas/4.plotting_bars.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+### We add this import!
+import matplotlib.pyplot as plt
+
+
+np.random.seed(1024)
+# plot siple time series data
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2017', periods=1000))
+part_sum = ts.cumsum()
+#part_sum.plot()
+#plt.show()
+
+# plot 4 plots, each for one column
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+print(df.head())
+
+df = df.cumsum()
+print(df.head())
+#df.plot()
+
+# other types of plots
+
+row1 = df.iloc[0]
+row2_6 = df.iloc[1:6]
+print(row2_6)
+
+#row1.plot(kind='bar')
+
+# row2_6.plot(kind='bar')
+# plt.axhline(0, color='k')
+#plt.show()
+
+bar_stacked = pd.DataFrame(np.random.rand(6, 4), columns=['a', 'b', 'c', 'd'])
+bar_stacked.plot.bar(stacked=True, title="Bar stacked plot");
+
+bar_stacked.plot.barh(stacked=True);
+plt.show()
